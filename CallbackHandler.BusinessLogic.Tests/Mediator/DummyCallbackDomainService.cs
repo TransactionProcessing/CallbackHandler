bcebc452f46f8579606f52fc7ed0f2f8ed5df536
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2023-01-01 12:38:23 +0000

    .

--- CallbackHandler.BusinessLogic.Tests/Mediator/DummyCallbackDomainService.cs
+++ CallbackHandler.BusinessLogic.Tests/Mediator/DummyCallbackDomainService.cs
@@ -15,6 +15,5 @@ public class DummyCallbackDomainService : ICallbackDomainService
                                      String reference,
                                      String[] destinations,
                                      CancellationToken cancellationToken) {
-            
     }
 }
