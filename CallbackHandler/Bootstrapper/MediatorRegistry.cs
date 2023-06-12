2343b7855aad366bda925ebd74c80a9f3b83b6b5
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2023-06-12 10:33:25 +0100

    Update shared nuget, and make aggregate a record, other nuget updates as well

--- CallbackHandler/Bootstrapper/MediatorRegistry.cs
+++ CallbackHandler/Bootstrapper/MediatorRegistry.cs
@@ -14,13 +14,8 @@ public class MediatorRegistry : ServiceRegistry
     public MediatorRegistry()
     {
         this.AddTransient<IMediator, Mediator>();
-
+        
         // request & notification handlers
-        this.AddTransient<ServiceFactory>(context =>
-                                          {
-                                              return t => context.GetService(t);
-                                          });
-
         this.AddSingleton<IRequestHandler<RecordCallbackRequest>, CallbackHandlerRequestHandler>();
         this.AddSingleton<ICallbackDomainService, CallbackDomainService>();
     }
