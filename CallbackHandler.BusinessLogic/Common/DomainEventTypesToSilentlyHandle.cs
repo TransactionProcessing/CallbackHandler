3be3395d24dc7cd061ede54c3970c5ccf16a7263
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2025-09-18 21:32:53 +0100

    :|

--- CallbackHandler.BusinessLogic/Common/DomainEventTypesToSilentlyHandle.cs
+++ CallbackHandler.BusinessLogic/Common/DomainEventTypesToSilentlyHandle.cs
@@ -52,5 +52,4 @@ public class DomainEventTypesToSilentlyHandle : IDomainEventTypesToSilentlyHandl
         }
 
         #endregion
-    }
 }
--- CallbackHandler/Bootstrapper/MiddlewareRegistry.cs
+++ CallbackHandler/Bootstrapper/MiddlewareRegistry.cs
@@ -86,5 +86,4 @@ public class MiddlewareRegistry :ServiceRegistry
 
             this.AddSingleton(config);
         }
-    }
 }
