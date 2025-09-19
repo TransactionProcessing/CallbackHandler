da9f6f74eb1f527a946c1142b3330dd4ee4e82b5
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2025-09-19 11:06:59 +0100

    :|

---
+++ CallbackHandler.IntegrationTests/Common/TestingContext.cs
@@ -0,0 +1,19 @@
+﻿using CallbackHandler.DataTransferObjects;
+using Shared.Logger;
+
+namespace CallbackHandler.IntegrationTests.Common;
+
+public class TestingContext
+{
+    public CallbackHandlerDockerHelper DockerHelper { get; set; }
+    public NlogLogger Logger { get; set; }
+
+    public List<Deposit> Deposits { get; set; }
+
+    public Dictionary<Guid, String> SentCallbacks { get; set; }
+
+    public TestingContext()
+    {
+        this.SentCallbacks = new Dictionary<Guid, string>();
+    }
+}
