06a41a359f1b8ec0252e4ba5b116ca3167f1344d
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2026-05-12 11:15:12 +0100

    fix failing tests

--- CallbackHandler/Common/JsonSerializerConfiguration.cs
+++ CallbackHandler/Common/JsonSerializerConfiguration.cs
@@ -13,46 +13,6 @@ public static class JsonSerializerConfiguration
         serializerOptions.DictionaryKeyPolicy = defaultOptions.DictionaryKeyPolicy;
         serializerOptions.ReferenceHandler = defaultOptions.ReferenceHandler;
         serializerOptions.WriteIndented = defaultOptions.WriteIndented;
-        serializerOptions.Converters.Add(new UtcDateTimeJsonConverter());
-        serializerOptions.Converters.Add(new NullableUtcDateTimeJsonConverter());
-    }
-
-    private sealed class UtcDateTimeJsonConverter : System.Text.Json.Serialization.JsonConverter<DateTime>
-    {
-        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-        {
-            var value = reader.GetDateTime();
-            return value.Kind == DateTimeKind.Utc ? value : value.ToUniversalTime();
-        }
-
-        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
-        {
-            writer.WriteStringValue(value.Kind == DateTimeKind.Utc ? value : value.ToUniversalTime());
-        }
-    }
-
-    private sealed class NullableUtcDateTimeJsonConverter : System.Text.Json.Serialization.JsonConverter<DateTime?>
-    {
-        public override DateTime? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-        {
-            if (reader.TokenType == JsonTokenType.Null)
-            {
-                return null;
-            }
-
-            var value = reader.GetDateTime();
-            return value.Kind == DateTimeKind.Utc ? value : value.ToUniversalTime();
-        }
-
-        public override void Write(Utf8JsonWriter writer, DateTime? value, JsonSerializerOptions options)
-        {
-            if (value.HasValue == false)
-            {
-                writer.WriteNullValue();
-                return;
-            }
-
-            writer.WriteStringValue(value.Value.Kind == DateTimeKind.Utc ? value.Value : value.Value.ToUniversalTime());
-        }
+        serializerOptions.Converters.Add(new DateTimeSpaceConverter());
     }
 }
